3d2e5e504e0e2a45306f24499beec79247eb3f52
hasan foraty <hasanforaty@gmail.com>
2023-09-07 13:27:34 +0330

    complete controller

--- src/main/java/com/saha/foraty/mycruddemo/controller/EmployeeRestController.java
+++ src/main/java/com/saha/foraty/mycruddemo/controller/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.saha.foraty.mycruddemo.controller;
 
 import com.saha.foraty.mycruddemo.entity.Employee;
 import com.saha.foraty.mycruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,38 @@ public class EmployeeRestController {
     List<Employee> getEmployees(){
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    Employee getEmployeeById(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee==null){
+            throw new RuntimeException("Employee id not found-"+employeeId);
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    Employee addEmployee(@RequestBody Employee theEmployee){
+        //make sure the employee is new one so it create new Employee
+        theEmployee.setId(0);
+        return employeeService.save(theEmployee);
+    }
+
+    @PutMapping("/employees")
+    Employee updateEmployee(@RequestBody Employee updatedEmployee){
+        return  employeeService.save(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    String  deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+        if(tempEmployee==null){
+            throw new RuntimeException("Employee is not found - "+employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "success";
+    }
+
+
+
 }
