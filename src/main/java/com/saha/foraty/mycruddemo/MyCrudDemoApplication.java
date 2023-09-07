b8de9a249009d71d95c324744c1a6dfad4dbcdad
hasan foraty <hasanforaty@gmail.com>
2023-09-07 08:40:33 +0330

    git init

---
+++ src/main/java/com/saha/foraty/mycruddemo/MyCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.saha.foraty.mycruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCrudDemoApplication.class, args);
+    }
+
+}
